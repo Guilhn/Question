b78fc75798fce36f9e7d5f649d361c29fb65b7e0
Guilherme Henrique Nascimento <gnascimento@gmail.com>
2019-04-17 15:02:02 -0300

    Foi alterado o caminho de relativo para éspecifico na raiz

--- Question/mvc/Visao/templates/menu.php
+++ Question/mvc/Visao/templates/menu.php
@@ -1,14 +1,14 @@
 <nav class="white" role="navigation">
   <div class="nav-wrapper container"><a id="logo-container" href="index" class="brand-logo">Question <img class="iconeLogo Cor" src="<?= URL_IMG . 'logo.svg' ?>"></a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="contato" class="black-text">Contato</a></li>
-      <li><a href="login" class="black-text">Login</a></li>
+      <li><a href="<?= URL_RAIZ . 'contato' ?>" class="black-text">Contato</a></li>
+      <li><a href="<?= URL_RAIZ . 'login' ?>" class="black-text">Login</a></li>
 
     </ul>
 
     <ul id="nav-mobile" class="sidenav">
-      <li><a href="contato" class="">Contato</a></li>
-      <li><a href="login" class="black-text">Login</a></li>
+      <li><a href="<?= URL_RAIZ . 'contato' ?>" class="">Contato</a></li>
+      <li><a href="<?= URL_RAIZ . 'login' ?>" class="black-text">Login</a></li>
     </ul>
     <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
   </div>
